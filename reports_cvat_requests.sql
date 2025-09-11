6b1beb1013deffdfd95c9d73e238cac0f70db195
YurkevichAndrei <ad6803884@yandex.ru>
2025-09-11 17:21:50 +0300

    add DB

---
+++ reports_cvat_requests.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Projects (id int primary key , name varchar(200));
+
+CREATE TABLE Stages (id int primary key , name varchar(50));
+
+CREATE TABLE States (id int primary key , name varchar(50));
