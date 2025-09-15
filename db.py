7e2461259225d00fabc39481ee6b64c494e3ff26
YurkevichAndrei <ad6803884@yandex.ru>
2025-09-15 17:42:14 +0300

    add init tasks, users, jobs

--- db.py
+++ db.py
@@ -15,6 +15,13 @@ class DataBase:
     #     self.network.close_session()
     #     self.db.close()
 
+    def __insert__(self, tabel_name, params, values):
+        params = ', '.join([p for p in params])
+        request = 'INSERT INTO %s (%s) VALUES %s' % (tabel_name, params, values)
+        cursor = self.db.cursor()
+        cursor.execute(request)
+        self.db.commit()
+
     def init_db_projects(self):
         if self.db is None:
             return False
@@ -25,14 +32,55 @@ class DataBase:
             if data == '':
                 separator = ''
             data = separator.join([data, '(%d, "%s")' % (project['id'], project['name'])])
+        self.__insert__('Projects', ['id', 'name'], data)
 
-        request = 'INSERT INTO Projects (id, name) VALUES %s' % data
-        cursor = self.db.cursor()
-        cursor.execute(request)
-# INSERT INTO Projects (id, name) VALUES
+    def init_db_tasks(self):
+        if self.db is None:
+            return False
+        tasks = self.network.get_tasks()['results']
+        data = ''
+        for task in tasks:
+            separator = ', '
+            if data == '':
+                separator = ''
+            data = separator.join([data, '(%d, "%s", %d)' % (task['id'], task['name'], task['project_id'])])
 
+        self.__insert__('Tasks', ['id', 'name', 'project_id'], data)
 
-# cursor.execute('')
+    def init_db_users(self):
+        if self.db is None:
+            return False
+        users = self.network.get_users()['results']
+        data = ''
+        for user in users:
+            separator = ', '
+            if data == '':
+                separator = ''
+            data = separator.join([data, '(%d, "%s", "%s", "%s", "%s")' % (user['id'], user['username'], user['email'], user['first_name'], user['last_name'])])
+
+        self.__insert__('Users', ['id', 'username', 'email', 'first_name', 'last_name'], data)
+
+    def init_db_jobs(self):
+        if self.db is None:
+            return False
+        stages = {'annotation': 0, 'validation': 1, 'acceptance': 2}
+        states = {'new': 0, 'in progress': 1, 'rejected': 2, 'completed': 3}
+        jobs = self.network.get_jobs()['results']
+        data = ''
+        for job in jobs:
+            separator = ', '
+            if data == '':
+                separator = ''
+            assignee = job['assignee']
+            assignee_id = -1
+            if not (assignee is None):
+                assignee_id = assignee['id']
+            data = separator.join([data, '(%d, %d, %d, %d, %d)' % (job['id'], assignee_id, stages[job['stage']], states[job['state']], job['task_id'])])
+
+        self.__insert__('Jobs', ['id', 'assignee', 'stage_id', 'state_id', 'task_id'], data)
 
 db = DataBase()
-db.init_db_projects()
+db.init_db_jobs()
+# db.init_db_users()
+# db.init_db_projects()
+# db.init_db_tasks()
--- reports_cvat_requests.sql
+++ reports_cvat_requests.sql
@@ -4,16 +4,17 @@ CREATE TABLE Stages (id int primary key , name varchar(50));
 
 CREATE TABLE States (id int primary key , name varchar(50));
 
-CREATE TABLE Users (id int primary key , username varchar(100), email varchar(100), password varchar(200));
+CREATE TABLE Users (id int primary key , username varchar(100), email varchar(100), first_name varchar(100), last_name varchar(100));
 
-CREATE TABLE Jobs (id int primary key ,
+CREATE TABLE Jobs (id int primary key,
                    assignee int,
                    stage_id int,
                    state_id int,
                    task_id int,
                    FOREIGN KEY (assignee) REFERENCES Users(id),
                    FOREIGN KEY (stage_id) REFERENCES Stages(id),
-                   FOREIGN KEY (state_id) REFERENCES States(id));
+                   FOREIGN KEY (state_id) REFERENCES States(id),
+                   FOREIGN KEY (task_id) REFERENCES Tasks(id));
 
 CREATE TABLE Tasks (id int primary key,
                     name varchar(200),
@@ -30,3 +31,27 @@ CREATE TABLE Reports (id int primary key,
                       shapes_count_today int,
                       shape_count_all int,
                       FOREIGN KEY (user_id) REFERENCES Users(id));
+
+INSERT INTO Stages (id, name) VALUES (0, 'annotation'), (1, 'validation'), (2, 'acceptance');
+INSERT INTO States (id, name) VALUES (0, 'new'), (1, 'in progress'), (2, 'rejected'), (3, 'completed');
+
+INSERT INTO Tasks (id, name, project_id) VALUES (3, "test_termo", 1), (2, "test2", 1), (1, "test", 1)
+
+ALTER TABLE Users RENAME COLUMN password TO first_name
+
+ALTER TABLE Users ADD COLUMN last_name
+
+ALTER TABLE Users DROP last_name
+
+INSERT INTO Users (id, username, email, first_name, last_name)
+VALUES (4, "abcabc", "abc@yandex.ru", "abc", "abc"),
+(3, "aaaaa", "a@mail.ru", "af", "al"),
+(2, "sdfghj", "sdfg@mail.ru", "jkjhkfgv", "fgfhgjnbhmn"),
+(1, "user", "a@a.ru", "", "")
+
+INSERT INTO Users (id, username, email, first_name, last_name)
+VALUES (-1, "-", "-", "-", "-")
+
+ALTER TABLE Jobs ADD CONSTRAINT fk_task_id_id FOREIGN KEY (task_id) REFERENCES Tasks(id)
+
+DROP TABLE "Jobs"
