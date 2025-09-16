7f9b149c7187bbb00114645e304419999e1744f2
YurkevichAndrei <ad6803884@yandex.ru>
2025-09-16 17:50:20 +0300

    update init database

--- db.py
+++ db.py
@@ -1,28 +1,42 @@
 import sqlite3
+import os
 
 from network import Networking
+# from parameters_selection import ParametersSelection
 
 class DataBase:
     def __init__(self):
         self.network = Networking()
         self.network.init_session()
-        try:
-            self.db = sqlite3.connect('reports_cvat.db')
-        except:
-            self.db = None
 
-    # def __del__(self):
-    #     self.network.close_session()
-    #     self.db.close()
+        db_file = 'reports_cvat__.db'
+        if os.path.isfile(db_file):
+            self.db = sqlite3.connect(db_file)
+        else:
+            self.db = sqlite3.connect(db_file)
+            self.init_db()
 
-    def __insert__(self, tabel_name, params, values):
+    def __del__(self):
+        self.network.close_session()
+        self.db.close()
+
+    def insert(self, table_name: str, params: list[str], values: str):
         params = ', '.join([p for p in params])
-        request = 'INSERT INTO %s (%s) VALUES %s' % (tabel_name, params, values)
+        request = 'INSERT INTO %s (%s) VALUES %s' % (table_name, params, values)
         cursor = self.db.cursor()
         cursor.execute(request)
         self.db.commit()
 
-    def __init_db__(self):
+    def select(self, table_name: str, columns: list[str], constraints: list[str] = ()):
+        request = f'SELECT {', '.join([c for c in columns])} FROM {table_name}'
+        if len(constraints) != 0:
+            constr = ' AND '.join([c for c in constraints])
+            request = ' WHERE '.join([request, constr])
+        cursor = self.db.cursor()
+        cursor.execute(request)
+        return cursor.fetchall()
+
+    def create_db(self):
         cursor = self.db.cursor()
 
         request = 'CREATE TABLE Projects (id int primary key , name varchar(200))'
@@ -87,69 +101,101 @@ class DataBase:
 
     # методы инициализаций таблиц нужно переделать под обновления,
     # чтобы можно было использовать при добавлении новых данных
-    def __init_db_projects__(self):
+    def update_db_projects(self):
         if self.db is None:
             return False
         projects = self.network.get_projects()['results']
+
+        only_in_id_list = self.difference_list_id('Projects', projects)
+
         data = ''
         for project in projects:
-            separator = ', '
-            if data == '':
-                separator = ''
-            data = separator.join([data, '(%d, "%s")' % (project['id'], project['name'])])
-        self.__insert__('Projects', ['id', 'name'], data)
+            if project['id'] in only_in_id_list:
+                separator = ', '
+                if data == '':
+                    separator = ''
+                data = separator.join([data, '(%d, "%s")' % (project['id'], project['name'])])
+        self.insert('Projects', ['id', 'name'], data)
 
-    def __init_db_tasks__(self):
+    def update_db_tasks(self):
         if self.db is None:
             return False
         tasks = self.network.get_tasks()['results']
+
+        only_in_id_list = self.difference_list_id('Tasks', tasks)
+
         data = ''
         for task in tasks:
-            separator = ', '
-            if data == '':
-                separator = ''
-            data = separator.join([data, '(%d, "%s", %d)' % (task['id'], task['name'], task['project_id'])])
+            if task['id'] in only_in_id_list:
+                separator = ', '
+                if data == '':
+                    separator = ''
+                data = separator.join([data, '(%d, "%s", %d)' % (task['id'], task['name'], task['project_id'])])
 
-        self.__insert__('Tasks', ['id', 'name', 'project_id'], data)
+        self.insert('Tasks', ['id', 'name', 'project_id'], data)
 
-    def __init_db_users__(self):
+    def update_db_users(self):
         if self.db is None:
             return False
         users = self.network.get_users()['results']
+
+        # нужно сначала получить полный список id, потом сравнить с результатами запроса
+        only_in_id_list = self.difference_list_id('Users', users)
+
         data = ''
         for user in users:
-            separator = ', '
-            if data == '':
-                separator = ''
-            data = separator.join([data, '(%d, "%s", "%s", "%s", "%s")' % (user['id'], user['username'], user['email'], user['first_name'], user['last_name'])])
+            if user['id'] in only_in_id_list:
+                separator = ', '
+                if data == '':
+                    separator = ''
+                data = separator.join([data, '(%d, "%s", "%s", "%s", "%s")' % (user['id'], user['username'], user['email'], user['first_name'], user['last_name'])])
 
-        self.__insert__('Users', ['id', 'username', 'email', 'first_name', 'last_name'], data)
+        self.insert('Users', ['id', 'username', 'email', 'first_name', 'last_name'], data)
 
-    def __init_db_jobs__(self):
+    def update_db_jobs(self):
         if self.db is None:
             return False
         stages = {'annotation': 0, 'validation': 1, 'acceptance': 2}
         states = {'new': 0, 'in progress': 1, 'rejected': 2, 'completed': 3}
         jobs = self.network.get_jobs()['results']
+
+        # нужно сначала получить полный список id, потом сравнить с результатами запроса
+        only_in_id_list = self.difference_list_id('Jobs', jobs)
+
         data = ''
         for job in jobs:
-            separator = ', '
-            if data == '':
-                separator = ''
-            assignee = job['assignee']
-            assignee_id = -1
-            if not (assignee is None):
-                assignee_id = assignee['id']
-            data = separator.join([data, '(%d, %d, %d, %d, %d)' % (job['id'], assignee_id, stages[job['stage']], states[job['state']], job['task_id'])])
+            # params = ParametersSelection()
+            # params.add_equal('id', job['id'], type(job['id']))
+            # constrains = params.get_parameters_selection()
+            # selection = self.__select__('Jobs', ['id'], constrains)
+            # # если объект по id найдет, то переходим на следующую итерацию
+            # if len(selection) == 0:
+            #     continue
+            if job['id'] in only_in_id_list:
+                separator = ', '
+                if data == '':
+                    separator = ''
+                assignee = job['assignee']
+                assignee_id = -1
+                if not (assignee is None):
+                    assignee_id = assignee['id']
+                data = separator.join([data, '(%d, %d, %d, %d, %d)' % (job['id'], assignee_id, stages[job['stage']], states[job['state']], job['task_id'])])
 
-        self.__insert__('Jobs', ['id', 'assignee', 'stage_id', 'state_id', 'task_id'], data)
+        self.insert('Jobs', ['id', 'assignee', 'stage_id', 'state_id', 'task_id'], data)
 
     def init_db(self):
-        self.__init_db__()
-        self.__init_db_projects__()
-        self.__init_db_tasks__()
-        self.__init_db_users__()
-        self.__init_db_jobs__()
+        self.create_db()
+        self.update_db_projects()
+        self.update_db_tasks()
+        self.update_db_users()
+        self.update_db_jobs()
+
+    def difference_list_id(self, table_name: str, objects):
+        id_list = sorted([obj['id'] for obj in objects])
+        selection = sorted(self.select(table_name, ['id']))
+        return set(id_list) - set(selection)
+
+#TODO дальше нужно реализовать формирования записей в таблице reports
 
 db = DataBase()
 db.init_db()
---
+++ parameters_selection.py
@@ -0,0 +1,34 @@
+class ParametersSelection:
+    def __init__(self):
+        self.params = []
+
+    def add_equal(self, field_name: str, value, value_type: type, symbol='='):
+        if value_type == int:
+            self.params.append({'field_name': field_name, 'symbol': symbol, 'value': value})
+        elif value_type == bool:
+            self.params.append({'field_name': field_name, 'symbol': symbol, 'value': str(int(value))})
+        else:
+            self.params.append({'field_name': field_name, 'symbol': symbol, 'value': f'"{value}"'})
+
+    def add_inequal(self, field_name: str, value, more_less: bool, and_equal: bool, value_type: type):
+        symbol = ''
+
+        if more_less:
+            symbol = '>'
+        else:
+            symbol = '<'
+
+        if and_equal:
+            symbol = f'{symbol}='
+
+        self.add_equal(field_name, value, value_type, symbol)
+
+    def get_parameters_selection(self):
+        result = []
+        for param in self.params:
+            result.append(f'{param['field_name']} {param['symbol']} {param['value']}')
+        return result
+
+# p = ParametersSelection()
+# p.add_inequal("1",False, False, True, bool)
+# print(p.params)
